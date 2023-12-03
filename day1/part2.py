238403ddf52031b67447bbf3167e7ee3916830ce
Ingvild Mjelva <33225998+imje@users.noreply.github.com>
2023-12-03 11:20:42 +0100

    Update part2.py

--- day1/part2.py
+++ day1/part2.py
@@ -14,25 +14,21 @@ nummer = {
 number_dict = {}
 total = 0
 
-with open('test.txt') as f:
+with open('input.txt') as f:
     for line in f:
         for i in line:
             if i.isdigit():
+                number_dict[line.find(i)] = i
                 number_dict[line.rfind(i)] = i
 
         #finne str
         for i in nummer:
             if i in line:
+                number_dict[line.find(i)] = nummer[i]
                 number_dict[line.rfind(i)] = nummer[i]
     
-#lage en dictionary og sortere den etter hvilket tall som kommer først og sist
-
         sorted_dict = dict(sorted(number_dict.items()))
-        print(sorted_dict)
-        print(list(sorted_dict.values())[0])
-        print(list(sorted_dict.values())[len(sorted_dict)-1])
         total += int(list(sorted_dict.values())[0]+list(sorted_dict.values())[len(sorted_dict)-1])
-        print(total)
         number_dict = {}
 
 print(total)
