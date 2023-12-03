0b26a96e154e9e33aa6fa825c86e111b6a6cebb7
Ingvild Mjelva <33225998+imje@users.noreply.github.com>
2023-12-03 13:36:27 +0100

    Create part2.py

---
+++ day2/part2.py
@@ -0,0 +1,42 @@
+the_power = 0
+
+
+def replace(line_text):
+    what_to_replace = ';:'
+    for i in what_to_replace:
+        if i in line_text:
+            line_text = line_text.replace(i, ',')
+    line_text = line_text.replace(' ', '')
+    return line_text
+
+def isNumber(entry):
+    full_number = ''
+    for i in entry:
+        if i.isdigit():
+            full_number += i
+    return int(full_number)
+
+#sjekk game for game
+with open('input.txt') as f:
+    for line in f:
+        is_ok = True
+        reds = []
+        blues = []
+        greens = []
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+        x = replace(line).split(',')
+
+#finner alle av hver farge
+        for i in x:
+            if ('red' in i):
+                reds.append(isNumber(i))
+            if ('green' in i):
+                greens.append(isNumber(i))
+            if ('blue' in i):
+                blues.append(isNumber(i))
+        
+        the_power += (max(blues)*max(greens)*max(reds))
+        
+print(the_power)
